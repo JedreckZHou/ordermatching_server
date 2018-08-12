0ede718d772a106d84ec12e332ee0cf54f2ff036
JedreckZHou <jedreckzhou@gmail.com>
2018-08-12 15:10:08 +0800

    initial submit

---
+++ src/main/java/com/training/ordermatching_server/OrdermatchingServerApplication.java
@@ -0,0 +1,12 @@
+package com.training.ordermatching_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermatchingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdermatchingServerApplication.class, args);
+    }
+}
